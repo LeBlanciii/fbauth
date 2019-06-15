6c56cd3eb8d1df3c216b0087a74cf5c83b5b7469
john <john@multimediallc.com>
2019-06-14 19:51:15 -0700

    initial

---
+++ fbauth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fbauth
+//
+//  Created by John LeBlanc on 6/9/19.
+//  Copyright © 2019 John LeBlanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
